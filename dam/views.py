e3e9a48a176e0b852e167d39837920c18752cf18
maximiliantitus <49737427+maximiliantitus@users.noreply.github.com>
2020-06-05 09:50:46 -0500

    a

--- dam/views.py
+++ dam/views.py
@@ -1,8 +1,5 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from .models import UserImage
-from keras.preprocessing.image import load_img, img_to_array
-import numpy as np
-from tensorflow.keras.applications.inception_resnet_v2 import InceptionResNetV2, decode_predictions, preprocess_input
 from PIL import Image
 from .forms import *
 
